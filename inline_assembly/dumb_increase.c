d11a24cfd05eb3e7a159da9e998b1beab29c20b8
Emode Ohimai <67082364+ohis-coder@users.noreply.github.com>
2026-06-29 22:42:53 +0100

    very irritating but easy transition to intel CISC, it is right to left

--- inline_assembly/dumb_increase.c
+++ inline_assembly/dumb_increase.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 
-    int dumb_increase(int old_value) {
-	int reformed_value = 0;
+int dumb_increase(int old_value) {
+  int reformed_value = 0;
 
-	// using ARM64 assembly to increment old value by 1 and return
-	// in reformed value!!! 
+  __asm__("addl $1, %1\n\t" // 1. Add the constant 1 directly to the input
+                            // register (%1)
+          "movl %1, %0\n\t" // 2. Handover: Copy that updated value into the
+                            // output register (%0)
+          : "=r"(reformed_value) // %0 maps cleanly to reformed_value
+          : "r"(old_value)       // %1 maps cleanly to old_value
+  );
 
-	// please note the function name, dumb increase...
-	// i am playing w assembly that is all 
-
-	__asm__( 
-		"mov w2, w1%\n\t"
-		"mov w3, w1\n\t"
-		"add w3, w3, #1\n\t"
-		"mov %w0, w3\n\t"
-		: "=r" (reformed_value)
-		: "r" (old_value)
-		: "x2", "x3"
-	);
-
-	return reformed_value;
-    }
+  return reformed_value;
+}
 
 int main() {
-dumb_increase(5);
+  int result = dumb_increase(5);
+  printf("Result of dumb_increase: %d\n", result); // Will print 6!
+  return 0;
 }
