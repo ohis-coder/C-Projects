05e674ecb0d8f13b71e03d86455643c2d3d2752f
Emode Ohimai <67082364+ohis-coder@users.noreply.github.com>
2026-07-01 16:27:53 +0100

    getnum function now bulletproof, pausing here cause i am not in any form of hurry lmao

--- ostep_c_repo/scheduling/fifo_scheduling/fifo.c
+++ ostep_c_repo/scheduling/fifo_scheduling/fifo.c
@@ -1,25 +1,30 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
 // this key is faulty on my keyboard so yea: 0 )
 
+int getnum() {
+  int i, c;
 
-int getnum(){
-	int i, c;
-	printf("Input Task Completion Time (< 9 secs bro)\n");
-	for (int i = 0; i < 1 && (c = getchar()) != EOF; i++) {
-		c - '0';
-		if (!isdigit(c) || c >= '9'){
-			printf("input a valid number dawg\n");
-			return -1;
-		}
-		i++;
-	}
-	return c;
+  printf("Input Task Completion Time (< 9 secs bro)\n");
+
+  c = getchar();
+
+  int next_char = getchar();
+
+  if (next_char != '\n' && next_char != EOF) {
+    printf("Single digits only bro\n");
+    return -1;
+  }
+  if (c == EOF || !isdigit(c) || c > '9') {
+    printf("Input a valid number between 0 - 9 dawg\n");
+    return -1;
+  }
+  return c - '0';
 }
 
-int main(){
-	int num = getnum(); 
-	printf("%d\n", num); 
-	return 0;
+int main() {
+  int num = getnum();
+  printf("%d\n", num);
+  return 0;
 }
